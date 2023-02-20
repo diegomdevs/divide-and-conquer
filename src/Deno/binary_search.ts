a478be79fea6c61774af568cfa978e7b668b72cc
Diego Gonzalez <diegom.develops@gmail.com>
2023-02-19 21:12:26 -0700

    feat: Deno: Binary Search
    Resolving binary_search problem using divide and conquer

--- src/Deno/binary_search.ts
+++ src/Deno/binary_search.ts
@@ -1,35 +1,16 @@
-export function binary_search(
-  list: any[],
-  target: any,
-  low = 0,
-  high: number
-): number {
-  if (low <= high) {
-    const mid = Math.floor((low + high) / 2);
-    const guess = list[mid];
-    if (guess != target) {
-      if (guess < target) low = mid + 1;
-      else high = mid - 1;
-      return binary_search(list, target, low, high);
-    } else return mid;
-  }
-  return -1;
-}
-
-export function binary_search_dac<T extends unknown>(
-  list: T[],
-  target: T
-): number {
-  if (list.length > 1) {
+export function binary_search(list: number[], target: number): number {
+  if (list.length != 0) {
     const low = 0;
     const high = list.length - 1;
     const mid = Math.floor((low + high) / 2);
     const guess = list[mid];
     if (guess != target) {
-      if (guess > target) return binary_search_dac(list.slice(0, mid), target);
-      else return binary_search_dac(list.slice(mid), target);
-    }
-    return mid;
+      if (guess > target) return binary_search(list.slice(0, mid), target);
+      else {
+        const result = binary_search(list.slice(mid + 1), target);
+        return result != -1 ? mid + 1 + result : -1;
+      }
+    } else return mid;
   }
   return -1;
 }
--- src/Deno/main.ts
+++ src/Deno/main.ts
@@ -1,6 +1,6 @@
 import { arrayItemSum, arrayItemSumDaC } from "./array_item_sum.ts";
 import { biggestListItem } from "./biggest_list_item.ts";
-import { binary_search_dac } from "./binary_search.ts";
+import { binary_search } from "./binary_search.ts";
 import { log_functions } from "./log_functions.ts";
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
@@ -9,5 +9,5 @@ if (import.meta.main) {
   log_functions(arrayItemSum([3, 3]), "List item sum");
   log_functions(biggestListItem(list, 1, list[0]), "Biggest Item in the list");
   console.log(arrayItemSumDaC([1, 2, 3, -2]));
-  console.log(binary_search_dac([1, 2, 3, 4, 5], 5));
+  console.log(binary_search([1, 2, 3, 4, 5], 5));
 }
